7600c7699e13da3f59eefa9572f4dda8251d5879
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-05-28 14:53:27 +0200

    Update 02_accessing_data.R

--- main/02_accessing_data.R
+++ main/02_accessing_data.R
@@ -1,8 +1,11 @@
 ## Accessing a single column
 # Using base and/or tidyverse functions
 df$Name # column one ('Name')
+
 df[, 1] # column one ('Name') # basically all rows, column one
+
 select(df, Name) # select column one ('Name)
+
 df[[1]] # column one ('Name)
 
 
@@ -11,6 +14,7 @@ df[[1]] # column one ('Name)
 df[1, ] # row one # basically row one, all columns
 
 # Using the data.table (DT) package
+library(data.table)
 first(dt) # only works for first row
 last(dt) # only works for last row
 
@@ -18,4 +22,5 @@ last(dt) # only works for last row
 ## Accessing a single cell
 # Using base and/or tidyverse functions
 df$Name[1] # cell in column one ('Name'), row one ['Alfred']
+
 df[1, 1] # cell in row one, column one ('Name') ['Alfred']
