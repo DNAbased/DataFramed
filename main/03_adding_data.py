bb13ccb0b3273bb17478382ac7f0fa72f30cdb04
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-05-28 14:45:10 +0200

    Add files via upload

---
+++ main/03_adding_data.py
@@ -0,0 +1,15 @@
+## Adding a single column
+# Using the pandas package
+import pandas as pd
+city = ['Bonn', 'London', 'Toronto', 'Quito', 'Reykjavik'] # new column to be added
+df['City'] = city # add city as new column
+df = df.assign(City = city) # assign city as new column
+df.insert(4, 'City', city) # insert city as new column to position 4 # note: happens inplace
+
+
+## Adding a single row
+# Using the pandas package
+import pandas as pd
+dict = {'Name': 'Fabulous', 'Age': 71, 'Random': 'Yes', 'Interesting': True} # dictionary of new row
+df = df.append(dict, ignore_index=True) # append dict to df # also works for appending another dataframe
+df.loc[5] = dict # add dict as new row # necessary to know, which index is the first 'empty' one
