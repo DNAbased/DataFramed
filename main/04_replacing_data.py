f243ae1a1ce044fb3f15048d3fdb7b6b2006b011
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-05-29 09:50:16 +0200

    Update 04_replacing_data.py

--- main/04_replacing_data.py
+++ main/04_replacing_data.py
@@ -1,18 +1,18 @@
 ## Replacing one column
-# Using the pandas package
+# Using pandas
 import pandas as pd
 df['Name'] = ['Argon', 'Balloon', 'Chess', 'Duck', 'Exit']
 df.Name = ['Argon', 'Balloon', 'Chess', 'Duck', 'Exit']
 
 
 ## Replacing on row
-# Using the pandas package
+# Using pandas
 import pandas as pd
 df.loc[0] = [1, False, 'Argon', 'No'] # not recommended # need to know the exact order of columns
 
 
 ## Replacing a single cell
-# Using the pandas package
+# Using pandas
 import pandas as pd
 df['Name'][0] = 'Argon' # not recommended # pandas warning
 df.loc[0, 'Name'] = 'Argon'
