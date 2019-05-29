6ac76f8f34e5d9aedfeb3731862800ca187ae622
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-05-29 14:10:45 +0200

    Update 04_replacing_data.py

--- main/04_replacing_data.py
+++ main/04_replacing_data.py
@@ -2,6 +2,7 @@
 # Using pandas
 import pandas as pd
 df['Name'] = ['Argon', 'Balloon', 'Chess', 'Duck', 'Exit']
+
 df.Name = ['Argon', 'Balloon', 'Chess', 'Duck', 'Exit']
 
 
@@ -15,7 +16,11 @@ df.loc[0] = ['Argon', 1, 'No', False] # not recommended # need to know the exact
 # Using pandas
 import pandas as pd
 df['Name'][0] = 'Argon' # not recommended # pandas warning
+
 df.loc[0, 'Name'] = 'Argon'
+
 df.at[0, 'Name'] = 'Argon'
+
 df.iloc[0, 0] = 'Argon' # note: this may be a different column due to pandas' ordering
+
 df.iat[0, 0] = 'Argon' # see above
