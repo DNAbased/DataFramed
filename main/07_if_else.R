368eb7ba6be6e6c941432a97826de295443173a9
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-04 09:03:24 +0200

    Add files via upload

---
+++ main/07_if_else.R
@@ -0,0 +1,9 @@
+## If - elif - else
+mambo = 5
+if (mambo != 5) {
+  print('missing')
+} else if (mambo == 5) {
+  print('correct')
+} else {
+  print('impossible')
+}
---
+++ main/07_if_else.py
@@ -0,0 +1,8 @@
+## If - elif - else
+mambo = 5
+if mambo != 5:
+    print('missing')
+elif mambo == 5:
+    print('correct')
+else:
+    print('impossible')
