c436a6918aabd46248ec7c344202f5ee94db8217
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-05 11:20:15 +0200

    Add files via upload

---
+++ main/08_for_loop.R
@@ -0,0 +1,15 @@
+## For loops
+for (i in 0:9) {
+  print(i)
+}
+
+food = c('Pizza', 'Steak', 'Salad')
+for (i in food) {
+  print(i)
+}
+
+for (i in 0:2) {
+  for (j in 0:2) {
+    print(i+j)
+  }
+}
