c76b927c547f327b144f4d9f331e9c003f44b20c
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-05 11:20:00 +0200

    Add files via upload

---
+++ main/08_for_loop.py
@@ -0,0 +1,11 @@
+## For loops
+for i in range(10):
+    print(i)
+
+food = ['Pizza', 'Steak', 'Salad']
+for i in food:
+    print(i)
+
+for i in range(3):
+    for j in range(3):
+        print(i+j)
