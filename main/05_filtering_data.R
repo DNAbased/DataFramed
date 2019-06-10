220ee1f6ea7c786380bce177522dbac7076ae595
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-10 12:05:30 +0200

    added dataset

--- main/05_filtering_data.R
+++ main/05_filtering_data.R
@@ -1,41 +1,48 @@
 ## Filtering based on values in a column
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df = df %>% filter(Age > 20)
 
 df = subset(df, Age > 20)
 
 # Using the data.table(DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 dt = dt[Age > 20]
 
 
 ## Drop specific columns
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df = df %>% select(-Age)
 
 df$Age = NULL
 
 # Using the data.table(DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 dt = dt[, Age:=NULL]
 
 
 ## Drop specific rows
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df[-1, ] # drops the first row
 
 
 ## Bonus: filter for NAs
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df[1, 1] = NA # set NA
 df %>% filter(is.na(Name)) # only NAs
 df %>% filter(!is.na(Name)) # only non-NAs
 
 # Using the data.table(DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 dt[1, 1] = NA # set NA
 dt[complete.cases(dt)]
