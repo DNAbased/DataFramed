668ad67ee2c0b1ef237bf5c4a49c960ddf2ee699
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-10 12:04:30 +0200

    added dataset

--- main/04_replacing_data.R
+++ main/04_replacing_data.R
@@ -1,6 +1,7 @@
 ## Replacing one column
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df$Name = c('Argon', 'Balloon', 'Chess', 'Duck', 'Exit')
 
 df[, 1] = c('Argon', 'Balloon', 'Chess', 'Duck', 'Exit')
@@ -11,11 +12,13 @@ df[[1]] = c('Argon', 'Balloon', 'Chess', 'Duck', 'Exit')
 ## Replacing one row
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df$Name = as.character(df$Name) # necessary to prevent NA in next step
 df[1, ] = c('Argon', 1, 'No', FALSE)
 
 # Using the data.table(DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 namelist = c('Argon', 'Balloon', 'Chess', 'Duck', 'Exit')
 dt[, Name := namelist]
 
@@ -23,6 +26,7 @@ dt[, Name := namelist]
 ## Replacing a single cell
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df$Name[1] = 'Argon'
 
 df[1, 1] = 'Argon'
