cd69caf76d83d8e3c982cd22cd2da6f823f4b6a0
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-10 12:04:51 +0200

    added dataset

--- main/04_replacing_data.py
+++ main/04_replacing_data.py
@@ -1,6 +1,7 @@
 ## Replacing one column
 # Using pandas
 import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 df['Name'] = ['Argon', 'Balloon', 'Chess', 'Duck', 'Exit']
 
 df.Name = ['Argon', 'Balloon', 'Chess', 'Duck', 'Exit']
@@ -9,12 +10,14 @@ df.Name = ['Argon', 'Balloon', 'Chess', 'Duck', 'Exit']
 ## Replacing one row
 # Using pandas
 import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 df.loc[0] = ['Argon', 1, 'No', False] # not recommended # need to know the exact order of columns
 
 
 ## Replacing a single cell
 # Using pandas
 import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 df['Name'][0] = 'Argon' # not recommended # pandas warning
 
 df.loc[0, 'Name'] = 'Argon'
