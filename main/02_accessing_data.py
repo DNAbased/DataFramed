4b39c6e6b46fd556d36f49e1faf225f705e6d284
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-10 11:47:18 +0200

    added dataset

--- main/02_accessing_data.py
+++ main/02_accessing_data.py
@@ -1,6 +1,7 @@
 ## Accessing a single column
 # Using pandas
 import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 df['Name'] # column one ('Name')
 
 df.Name # column one ('Name')
@@ -9,12 +10,14 @@ df.Name # column one ('Name')
 ## Accessing a single row
 # Using pandas
 import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 df.loc[0] # first row
 
 
 ## Accessing a single cell
 # Using pandas
 import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 df['Name'][0] # cell in column one ('Name'), row one ['Alfred']
 
 df.loc[0, 'Name'] # cell in row one, column one ('Name') ['Alfred']
