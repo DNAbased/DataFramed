1be624e18e9391cdfbdf7975e978fc61dc59453c
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-10 11:47:53 +0200

    added dataset

--- main/03_adding_data.R
+++ main/03_adding_data.R
@@ -1,6 +1,7 @@
 ## Adding a single column
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 city = c('Bonn', 'London', 'Toronto', 'Quito', 'Reykjavik')
 df$City = city
 
@@ -12,12 +13,14 @@ df = cbind(df, city); colnames(df[5]) <- 'City'
 
 # Using the data.table (DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 dt[, City := city]
 
 
 ## Adding a single row
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 new_row = data.frame('Name'='Fabulous', 'Age'=71, 'Random'='Yes', 'Interesting'=TRUE)
 df = bind_rows(df, new_row)
 
@@ -27,4 +30,5 @@ df = rbind(df, new_row)
 
 # Using the data.table (DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 NA # ?
