c37b5a0e2e3781ee29107045da9931bcc643f5f3
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-10 11:48:20 +0200

    added dataset

--- main/03_adding_data.py
+++ main/03_adding_data.py
@@ -1,6 +1,7 @@
 ## Adding a single column
 # Using pandas
 import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 city = ['Bonn', 'London', 'Toronto', 'Quito', 'Reykjavik'] # new column to be added
 df['City'] = city # add city as new column
 
@@ -12,6 +13,7 @@ df.insert(4, 'City', city) # insert city as new column to position 4 # note: hap
 ## Adding a single row
 # Using pandas
 import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 dict = {'Name': 'Fabulous', 'Age': 71, 'Random': 'Yes', 'Interesting': True} # dictionary of new row
 df = df.append(dict, ignore_index=True) # append dict to df # also works for appending another dataframe
 
