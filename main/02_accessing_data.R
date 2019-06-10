7231e40a286d2dcde3e714cc1559ff1de57d1bf9
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-10 11:46:48 +0200

    added dataset

--- main/02_accessing_data.R
+++ main/02_accessing_data.R
@@ -1,6 +1,7 @@
 ## Accessing a single column
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df$Name # column one ('Name')
 
 df[, 1] # column one ('Name') # basically all rows, column one
@@ -13,10 +14,12 @@ df[[1]] # column one ('Name)
 ## Accessing a single row
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df[1, ] # row one # basically row one, all columns
 
 # Using the data.table (DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 dt[, Name]
 first(dt) # only works for first row
 last(dt) # only works for last row
@@ -25,6 +28,7 @@ last(dt) # only works for last row
 ## Accessing a single cell
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df$Name[1] # cell in column one ('Name'), row one ['Alfred']
 
 df[1, 1] # cell in row one, column one ('Name') ['Alfred']
