e95b6c062e82d06aa0796681cdac70cd684bf44a
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-11 10:20:39 +0200

    added dataset

--- main/10_functions.R
+++ main/10_functions.R
@@ -1,4 +1,7 @@
 ## Functions
+library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
+
 df_wrangler = function(df, value=10) {
   random_number = round(runif(n = 1, min = 1, max = nrow(df)))
   random_row = df[random_number,]
@@ -8,3 +11,5 @@ df_wrangler = function(df, value=10) {
     return(paste(random_row$Name, ' is not interesting. BTW, you set the value as ', value, '.', sep=''))
   }
 }
+
+df_wrangler(df, 7)
