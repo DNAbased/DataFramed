205e0ecb45c15b0f1089c915c99394c8310ec85a
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-11 09:57:09 +0200

    added dataset

--- main/06_merging_data.R
+++ main/06_merging_data.R
@@ -1,6 +1,7 @@
 ## Adding new observations
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df2 = data.frame('Name' = c('Fabulous', 'Git', 'Hub'),
                  'Age' = c(71, 8, 19),
                  'Random' = c('Yes', 'Yes', 'No'),
@@ -9,6 +10,7 @@ df = rbind(df, df2)
 
 # Using the data.table(DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 dt2 = data.table('Name' = c('Fabulous', 'Git', 'Hub'),
                  'Age' = c(71, 8, 19),
                  'Random' = c('Yes', 'Yes', 'No'),
@@ -19,12 +21,14 @@ dt = rbindlist(list(dt, dt2), use.names=TRUE, fill=TRUE, idcol=NULL)
 ## Adding new variables (left join)
 # Using base and/or tidyverse functions
 library(tidyverse)
+df = read_tsv('00_R_data_set.tsv')
 df2 = data.frame('Name' = c('Alfred', 'Batman', 'Charlie', 'Delta', 'Echo'),
                  'City' = c('Bonn', 'London', 'Toronto', 'Quito', 'Reykjavik'))
 df %>% inner_join(df2, by = 'Name')
 
 # Using the data.table(DT) package
 library(data.table)
+dt = fread('00_R_data_set.tsv')
 dt2 = data.table('Name' = c('Alfred', 'Batman', 'Charlie', 'Delta', 'Echo'),
                  'City' = c('Bonn', 'London', 'Toronto', 'Quito', 'Reykjavik'))
 dt = dt[dt2, on='Name']
