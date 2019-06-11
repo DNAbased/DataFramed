8f1c6ca9f33e6196082333a0c69c227e2e242c90
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-11 10:21:15 +0200

    added dataset

--- main/10_functions.py
+++ main/10_functions.py
@@ -1,4 +1,7 @@
 ## Functions
+import pandas as pd
+df = pd.read_csv('00_py_data_set.tsv', sep='\t')
+
 def df_wrangler(df, value=10):
     import pandas as pd
     random_row = df.sample(n=1).reset_index()
@@ -6,3 +9,5 @@ def df_wrangler(df, value=10):
         print(random_row.at[0, 'Name'] + ' is interesting. BTW, you set the value as ' + str(value) + '.')
     else:
         print(random_row.at[0, 'Name'] + ' is not interesting. BTW, you set the value as ' + str(value) + '.')
+
+df_wranlger(df, 7)
