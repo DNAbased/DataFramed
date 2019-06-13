1885ae4b8e8da3242a448a150f5269eeb6ebc8cf
DNAbased <30318469+DNAbased@users.noreply.github.com>
2019-06-13 08:56:19 +0200

    Add files via upload

---
+++ main/13_plotting_data.R
@@ -0,0 +1,46 @@
+## Creating plotting data
+# Using base and/or tidyverse functions
+library(tidyverse)
+set.seed(65536)
+scatter_data = arrange(data.frame(Values = round(runif(100)*100), 
+                                  Random = rep(c('Yes', 'No', 'No', 'Yes', 'No'), 4)), 
+                       Values)
+bar_data = data.frame(Random = rep(c('Yes', 'No', 'No', 'Yes', 'No'), 4))
+histo_data = scatter_data
+box_data = scatter_data
+line_data = scatter_data
+pie_data = data.frame(Random = c('Yes', 'No'), Values = c(40, 60))
+
+## Plotting data
+# Using base and/or tidyverse functions
+library(tidyverse)
+# scatter_plot
+ggplot(scatter_data) + 
+  geom_point(aes(x=seq_along(Values), y=Values, colour=Random)) + 
+  labs(x='')
+
+# bar_plot
+ggplot(bar_data) + 
+  geom_bar(aes(x=Random, fill=Random)) + 
+  labs(y='')
+
+# histogram
+ggplot(histo_data) +
+  geom_histogram(aes(x=Values, fill=Random)) +
+  labs(y='')
+
+# box_plot
+ggplot(box_data) + 
+  geom_boxplot(aes(x=Random, y=Values, colour=Random)) + 
+  labs(x='')
+
+# line_plot
+ggplot(line_data) + 
+  geom_line(aes(x=seq_along(Values), y=Values, colour=Random)) + 
+  labs(x='')
+
+# pie_chart # but remember: pie charts are bad!
+ggplot(pie_data) + 
+  geom_bar(aes(x='', y=Values, fill=Random), stat='identity', width=1) + 
+  coord_polar('y') +
+  labs(x='', y='')
