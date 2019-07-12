e2e34e527c9e79743b424d32e1f8cbb43d296737
DNAbased <frederic.thieme@uni-bonn.de>
2019-07-12 13:22:25 +0200

    update

--- main/01_initializing_data.py
+++ main/01_initializing_data.py
@@ -1,5 +1,5 @@
 ## Creating a data set
-# Using pandas
+# pandas
 import pandas as pd
 dict = {'Name': ['Alfred', 'Batman', 'Charlie', 'Delta', 'Echo'],
 'Age': [99, 50, 30, 20, 0],
@@ -9,12 +9,12 @@ df = pd.DataFrame(dict)
 
 
 ## Reading in data
-# Using pandas
+# pandas
 import pandas as pd
 df = pd.read_csv('00_py_data_set.tsv', sep='\t')
 
 
 ## Writing data
-# Using pandas
+# pandas
 import pandas as pd
 df.to_csv('00_py_data_set_new.tsv', sep='\t', index=False)
